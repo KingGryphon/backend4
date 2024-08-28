ad70705642534451c67bad8fe60726a4e2844109
Archer Bergmann <archerbergmann@The-Kraken.ht.home>
2024-08-28 17:48:02 -0400

    tables

--- workspace/backend/restaurant/admin.py
+++ workspace/backend/restaurant/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-
+@admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
     list_display= ('Title', 'Price', 'Inventory')
     search_fields= ('Title', 'Price', 'id', 'Inventory')
 
-
+@admin.register(Booking)
 class BookoingAdmin(admin.ModelAdmin):
     list_display= ('Name', 'No_of_guests', 'BookingDate')
     search_fields= ('id', 'Name', 'No_of_guests', 'BookingDate')
